4c394ccd7e7dcb002422b0439f18f89e8b91864c
madniceautomotive <ps@madniceautomotive>
2026-06-07 20:39:46 +0200

    button rename png to label

--- assets/js/app.js
+++ assets/js/app.js
@@ -245,6 +245,7 @@ function renderManagerList(isInitialLoad = false) {
             }, index * 35);
         }
 
+        // UPGRADE: Text im Button von "PNG" zu "Label" geändert
         row.innerHTML = `
             <div class="ssd-row-header">
                 <div class="ssd-info-block">
@@ -264,7 +265,7 @@ function renderManagerList(isInitialLoad = false) {
                     </select>
                     <button class="print-btn" onclick="generateLabelPNG('${f.Name}', '${f.UUID}')">
                         <svg style="width:16px; height:16px; fill:currentColor;" viewBox="0 0 24 24"><path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/></svg>
-                        PNG
+                        Label
                     </button>
                 </div>
             </div>
@@ -449,7 +450,6 @@ function resetClearTimer() {
     }, 6000);
 }
 
-// Global an Window binden für den HTML-Klick-Trigger
 window.openScanner = openScanner;
 window.closeScanner = closeScanner;
 window.renderManagerList = renderManagerList;
