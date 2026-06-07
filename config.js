baf1fc6b6dcdced18491a7640942a5690f552cf5
madniceautomotive <ps@madniceautomotive.at>
2026-06-07 17:59:55 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Benenne diese Datei lokal in "config.js" um und füge deine echten Airtable-Daten ein.
-// Wichtig: Lade die "config.js" niemals auf GitHub hoch!
-
-const CONFIG = {
-    airtableToken: "pat4ytEWExJctNU62.59f8c764a353cf3d3571ea45e9d0d2e713e95a5a83499e97c5770f60850170b9", // Ersetze dies durch dein echtes Token falls regeneriert
-    baseId: "appXKM0UQ8uJLuiNB",
-    tableName: "SSDs"
-};
